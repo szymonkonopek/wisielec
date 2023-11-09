a7af7d2818535580c3b6dafa49c00bf0c41d457d
szymonkonopek <sk.gornik@gmail.com>
2023-11-09 13:45:57 +0100

    first commit

---
+++ Gra.py
@@ -0,0 +1,12 @@
+class Gra:
+    def __init__(self):
+        self.gra = True
+
+    def rozpocznijGre():
+        pass
+
+    def zakonczGre():
+        pass
+
+    def zgadnijLiterke():
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+import Gra;
+def main():
+    gra = Gra()
+    
+
+if __name__ == "__main__":
+    main()
